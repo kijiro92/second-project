4a1270037df95772faad11cf0b1a383b21643f2c
Steven Wingrove <kijiro.92@mac.com>
2016-09-27 23:48:19 -1000

    here

--- js/script.js
+++ js/script.js
@@ -1,5 +1,119 @@
 $(document).ready(function(){
 
+    $('p').hide();
 
+    $('.arrow-down').hide();
+
+    $('.arrow-up').hide();
+
+    $('.arrow-right').hide();
+
+    $('.arrow-left').hide();
+
+    $('.topBox').click(function(){
+
+        $('.text3').removeAttr("style");
+
+        $('.text2').removeAttr("style");
+
+        $('.text4').removeAttr("style");
+
+        $('p').hide();
+
+        $('.arrow-down').hide();
+
+        $('.arrow-up').hide();
+
+        $('.arrow-right').hide();
+
+        $('.arrow-left').hide();
+
+        $('.text1').css({"font-style": "italic" , "color":"#C4AA8E"});
+
+        $('.top').show("slow");
+
+        $('.arrow-down').show("fast")
+
+    });
+
+    $('.bottom').click(function(){
+
+        $('.text1').removeAttr("style");
+
+        $('.text2').removeAttr("style");
+
+        $('.text4').removeAttr("style");
+
+        $('p').hide();
+
+        $('.arrow-down').hide();
+
+        $('.arrow-up').hide();
+
+        $('.arrow-right').hide();
+
+        $('.arrow-left').hide();
+
+        $('.text3').css({"font-style": "italic" , "color":"#474747"});
+
+        $('.bottomBox').show("slow");
+
+        $('.arrow-up').show("fast")
+
+    });
+
+    $('.rightBox').click(function(){
+
+        $('.text1').removeAttr("style");
+
+        $('.text3').removeAttr("style");
+
+        $('.text4').removeAttr("style");
+
+        $('p').hide();
+
+        $('.arrow-down').hide();
+
+        $('.arrow-up').hide();
+
+        $('.arrow-right').hide();
+
+        $('.arrow-left').hide();
+
+        $('.text2').css({"font-style": "italic" , "color":"#424F5C"});
+
+        $('.right').show("slow");
+
+        $('.arrow-left').show("fast")
+
+    });
+
+    $('.leftBox').click(function(){
+
+        $('.text1').removeAttr("style");
+
+        $('.text2').removeAttr("style");
+
+        $('.text3').removeAttr("style");
+
+        $('p').hide();
+
+        $('.arrow-down').hide();
+
+        $('.arrow-up').hide();
+
+        $('.arrow-right').hide();
+
+        $('.arrow-left').hide();
+
+        $('.text4').css({"font-style": "italic" , "color":"#B22C15"});
+
+        $('.left').show("slow");
+
+        $('.arrow-right').show("fast")
+
+    });
 
 });
+
+
